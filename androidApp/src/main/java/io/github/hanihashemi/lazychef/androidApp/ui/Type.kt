d286f7388d50e8be02b0eb5044e0f42440504da6
Hani <jhanihashemi@gmail.com>
2020-10-31 20:46:55 +0100

    Change Theme.kt

--- androidApp/src/main/java/io/github/hanihashemi/lazychef/androidApp/ui/Type.kt
+++ androidApp/src/main/java/io/github/hanihashemi/lazychef/androidApp/ui/Type.kt
@@ -7,21 +7,42 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val typography = Typography(
+        h1 = TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Bold,
+                fontSize = 21.sp
+        ),
+        h2 = TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+        ),
+        subtitle1 = TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                color = silver
+        ),
         body1 = TextStyle(
                 fontFamily = FontFamily.Default,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp
+        ),
+        body2 = TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                color = silver
+        ),
+        caption = TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                color = silver
+        ),
+        button = TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.W500,
+                fontSize = 14.sp
         )
-        /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
