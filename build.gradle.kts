ade3f5d79a0fa6cf868c5a4a7616d5162f94c548
Hani <jhanihashemi@gmail.com>
2020-10-31 21:04:53 +0100

    add firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("com.android.tools.build:gradle:4.2.0-alpha14")
+        classpath("com.google.gms:google-services:4.3.4")
     }
 }
 group = "io.github.hanihashemi.lazychef"
