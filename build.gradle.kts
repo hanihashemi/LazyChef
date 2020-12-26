d5abf324095c77006848d98eb1f259501bba1a21
Hani <jhanihashemi@gmail.com>
2020-12-26 15:05:57 +0100

    Add ktlint plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,12 @@ buildscript {
         jcenter()
         google()
         mavenCentral()
+        maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("com.android.tools.build:gradle:4.2.0-alpha16")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.4.1")
         classpath("com.google.gms:google-services:4.3.4")
     }
 }
